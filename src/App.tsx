7c07d6f749e0cb2dfc4972fd02cf8d1e9673c4a0
Dimas Indra Jaya <dimaszxc101@gmail.com>
2023-06-08 12:32:24 +0700

    menambahkan data.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,41 @@
 import React from "react";
 import "./css/App.css";
 import "./css/main.css";
+import data from "./data.json";
+
+interface IBaseData {
+    cards: ICards[];
+    products: IProducts[];
+}
+
+interface ICards {
+    id: string;
+    headline: string;
+    content: string;
+}
+
+interface IProducts {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+}
 
 function App() {
     const [isOpen, setIsOpen] = React.useState(false);
 
+    const [responses] = React.useState<IBaseData>(data);
     const handleClick = () => {
         console.log("ke click");
         setIsOpen(!isOpen);
     };
+
+    // React.useEffect(() => {
+    //     console.log({ response });
+    //     console.log(response.cards);
+    //     console.log(response.products);
+    //     console.log("masuk");
+    // }, []);
 
     // const logo = require("./assets/img/baju.jpg");
 
@@ -101,49 +128,24 @@ function App() {
 
             <div className="container">
                 <div className="container-card">
-                    <div className="box">
-                        <span></span>
-                        <div className="content">
-                            <h2>Card Satu</h2>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur
-                                adipisicing elit. Quasi ipsum aliquid
-                                perferendis illo nostrum possimus exercitationem
-                                tempore beatae laudantium dolorem!
-                            </p>
-                            <a href="#">Read More</a>
-                        </div>
-                    </div>
-                    <div className="box">
-                        <span></span>
-                        <div className="content">
-                            <h2>Card Dua</h2>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur
-                                adipisicing elit. Quasi ipsum aliquid
-                                perferendis illo nostrum possimus exercitationem
-                                tempore beatae laudantium dolorem!
-                            </p>
-                            <a href="#">Read More</a>
-                        </div>
-                    </div>
-                    <div className="box">
-                        <span></span>
-                        <div className="content">
-                            <h2>Card Tiga</h2>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur
-                                adipisicing elit. Quasi ipsum aliquid
-                                perferendis illo nostrum possimus exercitationem
-                                tempore beatae laudantium dolorem!
-                            </p>
-                            <a href="#">Read More</a>
-                        </div>
-                    </div>
+                    {responses.cards.length > 0 ? (
+                        responses.cards.map((card) => (
+                            <div className="box">
+                                <span></span>
+                                <div className="content">
+                                    <h2>{card.headline}</h2>
+                                    <p>{card.content}</p>
+                                    <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <span>"cards is not existed yet"</span>
+                    )}
                 </div>
             </div>
 
-            <div className="grid">
+            {/* <div className="grid">
                 <div className="center">
                     <div className="outer">
                         <button>Read More</button>
@@ -151,102 +153,181 @@ function App() {
                         <span></span>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
             <div className="container-produk">
-                <div className="produk">
-                    <div className="card"></div>
-                    <div className="img-produk">
-                        <img src="./src/assets/img/baju.jpg" alt="" />
+                {responses.products.length > 0 ? (
+                    responses.products.map((product) => (
+                        <div className="produk">
+                            <div className="card"></div>
+                            <div className="img-produk">
+                                <img src="./src/assets/img/baju.jpg" alt="" />
+                            </div>
+                            <div className="content">
+                                <div className="details">
+                                    <h2>{product.name}</h2>
+                                    <span>{product.description}</span>
+                                    <div className="data">
+                                        <h3>Rp. {product.price}</h3>
+                                    </div>
+                                    <div className="dataBtn">
+                                        <button>Read More!</button>
+                                        <button>Buy Now!</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <span>"cards is not existed yet"</span>
+                )}
+            </div>
+
+            <div className="kotak">
+                <div className="kotak-card">
+                    <div className="card">
+                        <button>Read More</button>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
                     </div>
-                    <div className="content">
-                        <div className="details">
-                            <h2>Baju Bolong</h2>
-                            <span>Merk Macan Tutul</span>
-                            <div className="data">
-                                <h3>Rp. 15.000,00,-</h3>
+                </div>
+                <div className="kotak-isi">
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <div className="isi">
+                                <h3>Judul</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Ad dignissimos nisi cum
+                                    assumenda fugit unde soluta error nesciunt
+                                    natus impedit!
+                                </p>
                             </div>
-                            <div className="dataBtn">
-                                <button>Read More!</button>
-                                <button>Buy Now!</button>
-                            </div>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="kotak-card-isi">
+                        <div className="card-isi">
+                            <h3>Judul</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Ad dignissimos nisi cum
+                                assumenda fugit unde soluta error nesciunt natus
+                                impedit!
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="produk">
-                    <div className="card"></div>
-                    <div className="img-produk">
-                        <img src="./src/assets/img/baju.jpg" alt="" />
-                    </div>
-                    <div className="content">
-                        <div className="details">
-                            <h2>Baju Bolong</h2>
-                            <span>Merk Macan Tutul</span>
-                            <div className="data">
-                                <h3>Rp. 15.000,00,-</h3>
-                            </div>
-                            <div className="dataBtn">
-                                <button>Read More!</button>
-                                <button>Buy Now!</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="produk">
-                    <div className="card"></div>
-                    <div className="img-produk">
-                        <img src="./src/assets/img/baju.jpg" alt="" />
-                    </div>
-                    <div className="content">
-                        <div className="details">
-                            <h2>Baju Bolong</h2>
-                            <span>Merk Macan Tutul</span>
-                            <div className="data">
-                                <h3>Rp. 15.000,00,-</h3>
-                            </div>
-                            <div className="dataBtn">
-                                <button>Read More!</button>
-                                <button>Buy Now!</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="produk">
-                    <div className="card"></div>
-                    <div className="img-produk">
-                        <img src="./src/assets/img/baju.jpg" alt="" />
-                    </div>
-                    <div className="content">
-                        <div className="details">
-                            <h2>Baju Bolong</h2>
-                            <span>Merk Macan Tutul</span>
-                            <div className="data">
-                                <h3>Rp. 15.000,00,-</h3>
-                            </div>
-                            <div className="dataBtn">
-                                <button>Read More!</button>
-                                <button>Buy Now!</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="produk">
-                    <div className="card"></div>
-                    <div className="img-produk">
-                        <img src="./src/assets/img/baju.jpg" alt="" />
-                    </div>
-                    <div className="content">
-                        <div className="details">
-                            <h2>Baju Bolong</h2>
-                            <span>Merk Macan Tutul</span>
-                            <div className="data">
-                                <h3>Rp. 15.000,00,-</h3>
-                            </div>
-                            <div className="dataBtn">
-                                <button>Read More!</button>
-                                <button>Buy Now!</button>
-                            </div>
-                        </div>
+            </div>
+
+            <div className="kotak">
+                <div className="kotak-card">
+                    <div className="card">
+                        <button>Read More</button>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
                     </div>
                 </div>
             </div>
